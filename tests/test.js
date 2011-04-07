b17ccc8ea35f3acba2ef1a5851e0ccf4bc99d00f
Morteza Milani <mrtz.milani@googlemail.com>
2011-04-07 18:37:40 +0430

    readFiles and readDirectory functions added.
    Now you can read all files in a directory using readFiles.
    Also you can list directory files and folders using readDirectory.

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2010 Morteza Milani
+ * Copyright (c) 2011 Morteza Milani
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
