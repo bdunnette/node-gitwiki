d842d1773abc3bbcab34b2c6e427ca25e7906e78
Morteza Milani <mrtz.milani@googlemail.com>
2011-08-25 16:11:16 +0430

    Fixes #1

--- tests/test.js
+++ tests/test.js
@@ -3,23 +3,23 @@ var gitwiki = require('../lib/gitwiki');
 var REPO_PATH = '../examples/wiki';
 
 console.log('Opening wiki repository');
-var wiki = gitwiki.open(REPO_PATH);
+var wiki = gitwiki.openSync(REPO_PATH);
 console.log('Wiki opened successfully');
 
 console.log('Page listing: ');
-console.log(wiki.listPages());
+console.log(wiki.listPagesSync());
 
 console.log('Load page contents (FAQ):');
-console.log(wiki.loadPage('FAQ'));
+console.log(wiki.loadPageSync('FAQ'));
 
 console.log('History of "Installation Guide" page (5 most recent changes): ');
-console.log(wiki.pageHistory('installation-guide',5));
+console.log(wiki.pageHistorySync('installation-guide',5));
 
 console.log('Wiki History (5 most recent changes): ');
-console.log(wiki.history(5));
+console.log(wiki.historySync(5));
 
 console.log('Compare revisions (two last commits): ');
-console.log(wiki.compareRevisions('3e59fb20f25dd4d3075628145355a2f0444f07a9',
+console.log(wiki.compareRevisionsSync('3e59fb20f25dd4d3075628145355a2f0444f07a9',
                                   '98f922bf026b550eb0dc4794fde81694b281ea82',
                                   {
                                     withContents : true
